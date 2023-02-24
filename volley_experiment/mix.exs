a94062f63e3539db9cbcb84b18960906dca5c0a1
charlieroth <charlieroth4@icloud.com>
2023-02-24 10:29:24 +0100

    volley-exp: add deps

--- volley_experiment/mix.exs
+++ volley_experiment/mix.exs
@@ -11,7 +11,6 @@ defmodule VolleyExperiment.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +18,11 @@ defmodule VolleyExperiment.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:volley, "~> 1.0"},
+      {:broadway, "~> 1.0"},
+      {:jason, "~> 1.4"}
     ]
   end
 end
