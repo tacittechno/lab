9f7497431cfb9f714c6dab83635bdb3a038b9efc
charlieroth <charlieroth4@icloud.com>
2023-02-26 16:37:36 +0100

    otp_lab: init

---
+++ otp_lab/lib/otp_lab.ex
@@ -0,0 +1,18 @@
+defmodule OtpLab do
+  @moduledoc """
+  Documentation for `OtpLab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OtpLab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ otp_lab/test/otp_lab_test.exs
@@ -0,0 +1,8 @@
+defmodule OtpLabTest do
+  use ExUnit.Case
+  doctest OtpLab
+
+  test "greets the world" do
+    assert OtpLab.hello() == :world
+  end
+end
