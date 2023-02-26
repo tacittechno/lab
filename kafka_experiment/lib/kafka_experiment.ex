b2d5b31cf68a708f32d05fed36b1e3b566c2eb0c
charlieroth <charlieroth4@icloud.com>
2023-02-26 12:57:31 +0100

    kafka_exp: clean up

--- kafka_experiment/lib/kafka_experiment.ex
+++ kafka_experiment/lib/kafka_experiment.ex
@@ -1,18 +1,2 @@
 defmodule KafkaExperiment do
-  @moduledoc """
-  Documentation for `KafkaExperiment`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KafkaExperiment.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
